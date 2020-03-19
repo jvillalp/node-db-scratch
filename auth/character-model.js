7fade968ca4fd9f9c24d910e958c91245892804c
Juana Villalpando <juana.v14@gmail.com>
2020-03-19 16:03:25 -0700

    create character model

--- auth/character-model.js
+++ auth/character-model.js
@@ -1,33 +1,47 @@
-const db = require('../database/dbConfig')
+const db = require("../database/dbConfig");
 
 module.exports = {
-    add,
-    find, 
-    findBy,
-    findById,
-    update,
-    remove
+  add,
+  find,
+  findBy,
+  findById,
+  update,
+  remove
 };
 
-function add (){
+// async function add (character){
+//     const [id] = await db('characters')
+//     .insert(character)
+//     return findById(id)
+// }
 
+function add(character) {
+  return db("characters").insert(character);
 }
 
-function find(){
-    return db('characters')
-}
-function findBy(){
-
+function find() {
+  return db("characters");
 }
 
-function findById(){
-
+//query selector by name, etc.
+function findBy(filter) {
+  return db("characters").where(filter);
 }
 
-function update(){
-
+function findById(id) {
+  return db("characters")
+    .where({ id })
+    .first();
 }
 
-function remove(){
+function update(id, changes) {
+  return db("characters")
+    .where({ id })
+    .update(changes);
+}
 
+function remove(id) {
+  return db("characters")
+    .where({ id })
+    .del();
 }
--- auth/character-router.js
+++ auth/character-router.js
@@ -5,10 +5,10 @@ const Characters = require("./character-model.js");
 router.get("/", (req, res) => {
   Characters.find()
     .then(character => {
-        res.status(200).json(character)
+      res.status(200).json(character);
     })
     .catch(err => {
-        res.status(500).json({ errormessage: `${err}`})
+      res.status(500).json({ errormessage: `${err}` });
     });
 });
 module.exports = router;
